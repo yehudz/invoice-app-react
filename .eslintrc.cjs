77f29638242e6f290ed3c61f2c375fa39b962018
yehudz <yehuda.zeytim@gmail.com>
2022-09-12 23:48:52 -0600

    build: implement prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,26 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
+  extends: ['plugin:react/recommended', 'standard-with-typescript'],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json'
+    project: './tsconfig.json',
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ['react'],
   rules: {
-    "no-unused-vars": [1, { "args": "after-used", "argsIgnorePattern": "^_" }],
-    "space-before-blocks": "off",
-    "quotes": "off",
-    "semi": "off",
-    "no-extra-semi": "off"
+    'no-unused-vars': [1, { args: 'after-used', argsIgnorePattern: '^_' }],
+    'space-before-blocks': 'off',
+    quotes: 'off',
+    semi: 'off',
+    'no-extra-semi': 'off',
   },
-  "settings": {
-    "react": {
-      "version": "detect"
-    }
-  }
-}
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
