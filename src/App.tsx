c164c57c383a9bb40a96be5a14fa5ac1e82a591d
yehudz <yehuda.zeytim@gmail.com>
2022-09-13 01:46:50 -0600

    build: new eslint setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import './App.css'
 
-function App (): JSX.Element {
-  return <></>
+function App(): JSX.Element {
+  function test(): void {
+  }
+  test()
+  return (
+    <>
+      <h1>Hello from eslint hell</h1>
+    </>
+  )
 }
 
 export default App
