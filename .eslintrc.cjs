94b149e7b8f161326ac1742f7e756d6b89853b6f
yehudz <yehuda.zeytim@gmail.com>
2022-09-12 22:32:41 -0600

    build: implement prettier and eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'standard-with-typescript'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'no-unused-vars': [1, { args: 'after-used', argsIgnorePattern: '^_' }],
+  },
+};
