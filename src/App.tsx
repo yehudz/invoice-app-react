bb20732cf7132d12d6227c3c60e0a3dfb8c7fa80
yehudz <yehuda.zeytim@gmail.com>
2022-09-13 13:33:46 -0600

    style: implement colors and font family
    Add resources for local fonts
    Add colors to tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ function App(): JSX.Element {
   test()
   return (
     <>
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
+       <h1 className="text-3xl font-bold">
+      Invoice App
     </h1>
     </>
   )
